1c7d2c6ad2ad02a7bdf937bb36e040fb305f6feb
Austinia <kimdy90919@gmail.com>
2022-04-20 17:46:07 +0900

    공지사항 일부 완성

--- server/server.js
+++ server/server.js
@@ -2,23 +2,23 @@ const express = require("express");
 //Cross-Origin Resource Sharing, 서버와 클라의 통신을 위한 모듈
 const cors = require("cors");
 const port = process.env.PORT || 3030;
-const app = express();
-app.use(cors());
+const server = express();
+server.use(cors());
 
 //공지사항 모듈
 const announcement = require("./announcement");
 
 /* -----공지사항----- */
-app.get("/api/announcement", announcement.show);
-app.post("/api/customers", function (req, res) {
+server.get("/api/announcement", announcement.show);
+server.post("/api/announcement", function (req, res) {
   announcement.create;
 });
-app.delete("/api/customers/:id", announcement.delete);
-app.post("/api/customers/:id", function (req, res) {
+server.delete("/api/announcement/:id", announcement.delete);
+server.post("/api/announcement/:id", function (req, res) {
   announcement.update;
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

702d7481f9ecfeebe721686ef23639f6669507bb
Austinia <kimdy90919@gmail.com>
2022-05-20 21:41:37 +0900

    전 커밋 살짝 수정

--- server/auth/signout.js
+++ server/auth/signout.js
@@ -12,15 +12,23 @@ exports.signout = async (req, res) => {
   const connection = await getConnection();
   const sql =
     "UPDATE member SET refresh_token = null WHERE idx = ? AND enabled = 0";
-  const [rows, fields] = await connection.query(sql, idx);
-  if (rows.affectedRows == 1) {
-    res.status(200).json({
-      code: "204",
-      message: "로그아웃 되었습니다.",
-    });
-  } else if (rows.affectedRows > 1) {
-    res.status(409).json({ status: "409", message: "광역 로그아웃 에러" });
-  } else {
+  try {
+    const [rows, fields] = await connection.query(sql, idx);
+    if (rows.affectedRows == 1) {
+      res.status(200).json({
+        code: "204",
+        message: "로그아웃 되었습니다.",
+      });
+    } else if (rows.affectedRows > 1) {
+      res.status(409).json({ status: "409", message: "광역 로그아웃 에러" });
+    } else {
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러",
+      });
+    }
+  } catch (error) {
+    console.error(error);
     return res.status(500).json({
       code: 500,
       message: "서버 에러",

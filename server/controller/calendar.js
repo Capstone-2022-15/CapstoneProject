d16365e104b6057bc1599646f9b10624df53dabe
Austinia <kimdy90919@gmail.com>
2022-05-22 19:59:18 +0900

    클라이언트 파일 초기화

--- server/controller/calendar.js
+++ server/controller/calendar.js
@@ -3,6 +3,7 @@ const { getConnection } = require("../model/getConnection");
 exports.calendar = async (req, res) => {
   try {
     const connection = await getConnection();
+    //일정이 있는 게시물만 SELECT
     const sql = `SELECT A.idx, A.config_idx, A.subject, A.startDate, A.finalDate FROM board as A WHERE A.startDate IN (SELECT B.startDate FROM board as B) AND isDeleted = 0`;
     const [rows, field] = await connection.query(sql);
     res.status(200).json({

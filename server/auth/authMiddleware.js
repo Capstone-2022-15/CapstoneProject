49cfb6fa6c861ae6f2b8d8f03c4d0470ed1595df
Austinia <kimdy90919@gmail.com>
2022-05-23 00:13:33 +0900

    인증 Hotfix
    유효하지 않은 토큰일 시 무응답 해결

--- server/auth/authMiddleware.js
+++ server/auth/authMiddleware.js
@@ -13,6 +13,7 @@ exports.auth = async (req, res, next) => {
     req.decoded = jwt.verify(req.headers.authorization, SECRET_KEY);
     return next();
   } catch (error) {
+    console.log(error);
     /* 인증 실패 */
     // 유효시간이 초과된 경우
     if (error.name === "TokenExpiredError") {
@@ -59,13 +60,13 @@ exports.auth = async (req, res, next) => {
           });
         }
       }
-      // 토큰의 비밀키가 일치하지 않는 경우
-      if (error.name === "JsonWebTokenError") {
-        return res.status(401).json({
-          code: 401,
-          message: "유효하지 않은 토큰입니다.",
-        });
-      }
+    }
+    // 토큰의 비밀키가 일치하지 않는 경우
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        code: 401,
+        message: "유효하지 않은 토큰입니다.",
+      });
     }
   }
 };

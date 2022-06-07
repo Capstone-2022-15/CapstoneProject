14bbb798a4f0ef8c75b4a00e8e97c66f4915fb0a
kkang123 <85389685+kkang123@users.noreply.github.com>
2022-06-07 10:30:11 +0900

    jpg 출력

--- kakao_date jpg.py
+++ kakao_date jpg.py
@@ -110,10 +110,6 @@ def main():
             # 자른 이미지 출력
             cut_img
 
-          # 양 끝에 하이폰 있으면 제거
-          word = word.strip('-')
-          print(word)
-
     else:
        print('사진 인식 실패')
 
